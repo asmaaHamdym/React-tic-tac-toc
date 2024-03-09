402c846b59f2054e59a5f8559740b788c1b87ee5
asmaaHamdym <asmahamdym@gmail.com>
2024-03-10 00:56:30 +0200

    Add interactivity to the tic-tac-toe board by adding the ability to click on squares and update the board state.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
+import { useState } from "react";
+
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <button onClick={onSquareClick} className="square">
+      {value}
+    </button>
+  );
+};
 export default function Board() {
+  const handleSquareClick = (i) => {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  };
+  const [squares, setSquares] = useState(Array(9));
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square onSquareClick={() => handleSquareClick(0)} value={squares[0]} />
+        <Square onSquareClick={() => handleSquareClick(1)} value={squares[1]} />
+        <Square onSquareClick={() => handleSquareClick(2)} value={squares[2]} />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square onSquareClick={() => handleSquareClick(3)} value={squares[3]} />
+        <Square onSquareClick={() => handleSquareClick(4)} value={squares[4]} />
+        <Square onSquareClick={() => handleSquareClick(5)} value={squares[5]} />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square onSquareClick={() => handleSquareClick(6)} value={squares[6]} />
+        <Square onSquareClick={() => handleSquareClick(7)} value={squares[7]} />
+        <Square onSquareClick={() => handleSquareClick(8)} value={squares[8]} />
       </div>
     </>
   );
